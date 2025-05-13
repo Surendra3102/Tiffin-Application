773a4231838417f035c85f79c461d0d88d97d36b
Surendra <surendrabsvm005@gmail.com>
2025-05-13 22:23:24 +0530

    create super user

--- build.sh
+++ build.sh
@@ -2,7 +2,12 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --noinput
+python manage.py collectstatic --no-input
 
 python manage.py migrate
-python manage.py makemigrations
+python manage.py makemigrations 
+
+if [[$CREATE_SUPERUSER]];
+then
+    python manage.py createsuperuser --no-input
+fi
